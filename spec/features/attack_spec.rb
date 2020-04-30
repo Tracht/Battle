65fd000350bec885b067a3f6bfb41704ad673b9c
Hibo Abdilaahi <51047911+hiboabd@users.noreply.github.com>
2020-04-30 18:24:06 +0100

    Created message to say who got attacked

--- spec/features/attack_spec.rb
+++ spec/features/attack_spec.rb
@@ -2,14 +2,14 @@ feature 'Attack' do
   scenario 'Player 1 attacks player 2' do
     sign_in_and_play
     click_button('Attack')
-    expect(page).to have_content 'Success! Player 2 attacked!'
+    expect(page).to have_content 'Success! Steve attacked!'
   end
 
   feature 'View hit points' do
     scenario 'see both players hit points' do
       sign_in_and_play
       click_button('Attack')
-      expect(page).to have_content 'Success! Player 2 attacked! John : 100 HP Steve : 90 HP'
+      expect(page).to have_content 'Success! Steve attacked! John : 100 HP Steve : 90 HP'
     end
   end
 
